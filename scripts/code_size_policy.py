c5f0a80f94f208cef7f7ff9225cae9d3200d848f
Dennis Liu <dennis48161025@gmail.com>
2026-07-16 03:34:40 +0800

    refactor(domain): share byte range subtraction

--- scripts/code_size_policy.py
+++ scripts/code_size_policy.py
@@ -63,11 +63,11 @@ class CodeSizeSnapshot:
 
 
 DEFAULT_LIMITS = CodeSizeLimits(
-    production_nonblank=57_696,
+    production_nonblank=57_665,
     duplicate_json_nonblank=1_156,
     partial_type_default_max=2_500,
     partial_type_exact_ratchets={
-        "NvtFwCombiner.Bootstrap.WorkbenchCompositionService": 5_035,
+        "NvtFwCombiner.Bootstrap.WorkbenchCompositionService": 5_002,
         "NvtFwCombiner.Presentation.Avalonia.ViewModels.MainWindowViewModel": 3_029,
     },
 )
