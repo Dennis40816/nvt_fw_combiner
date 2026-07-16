94ce865d20b87f056633008aa6919292dd7807e1
Dennis Liu <dennis48161025@gmail.com>
2026-07-16 08:06:11 +0800

    chore(size): ratchet review follow-up

--- scripts/code_size_policy.py
+++ scripts/code_size_policy.py
@@ -63,12 +63,12 @@ class CodeSizeSnapshot:
 
 
 DEFAULT_LIMITS = CodeSizeLimits(
-    production_nonblank=56_902,
+    production_nonblank=56_901,
     duplicate_json_nonblank=1_156,
     partial_type_default_max=2_500,
     partial_type_exact_ratchets={
         "NvtFwCombiner.Bootstrap.WorkbenchCompositionService": 4_483,
-        "NvtFwCombiner.Presentation.Avalonia.ViewModels.MainWindowViewModel": 2_848,
+        "NvtFwCombiner.Presentation.Avalonia.ViewModels.MainWindowViewModel": 2_847,
     },
 )
 
